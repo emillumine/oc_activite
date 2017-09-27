b045d3e0a05a93bbe9884e8e6e69e9f0e7bc1dca
camille <camille@Oroshi.home>
2017-09-27 08:08:52 +0200

    Modification du logout avec la methode delete, plus propre.

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,7 @@ Rails.application.routes.draw do
 
   get 'users/login' => 'users#login'
   post 'users/login' => 'users#check'
-
-  get 'users/logout' => 'users#logout'
+  delete 'users/login' => 'users#logout'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
